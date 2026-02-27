f6b666e6088891f918dfc520e832cb5f4406d4b8
ezekielreu6-bit <ezekielreu6@gmail.com>
2026-02-27 16:06:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns:[
-      { protocol: 'https', hostname: 'api.dicebear.com' },
-      { protocol: 'https', hostname: 'res.cloudinary.com' },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // For Google Auth avatars
+        pathname: '**',
+      },
     ],
   },
 };
+
 module.exports = nextConfig;
